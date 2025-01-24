d8163cf8fb1c172344f005f4925512d315e260cc
394049428@qq.com <394049428@qq.com>
2025-01-24 15:08:55 +0800

    : ⬆️ 升级依赖，支持新版idea

--- src/main/java/com/github/chencn/yamlandprops/ConvertAction.java
+++ src/main/java/com/github/chencn/yamlandprops/ConvertAction.java
@@ -8,6 +8,7 @@ import com.github.chencn.yamlandprops.yaml2props.Yaml2Props;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
@@ -25,16 +26,23 @@ import java.io.IOException;
 public class ConvertAction extends AnAction {
 
     @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return ActionUpdateThread.BGT;
+    }
+
+    @Override
     public void update(@NotNull AnActionEvent event) {
         super.update(event);
         Presentation presentation = event.getPresentation();
         // get file type
         final String fileType = CommonUtils.getFileType(event, false);
-        //根据类型动态控制Action的隐藏显示
-        if (!StringUtil.isEmpty(fileType) && presentation.isEnabled()) {
-            event.getPresentation().setEnabledAndVisible(Constant.YAML.equals(fileType) || Constant.PROPERTIES.equals(fileType));
+        // 根据类型动态控制Action的隐藏显示
+        if(!StringUtil.isEmpty(fileType) && presentation.isEnabled()) {
+            event.getPresentation()
+                    .setEnabledAndVisible(Constant.YAML.equals(fileType) || Constant.PROPERTIES.equals(fileType));
         } else {
-            event.getPresentation().setEnabledAndVisible(false);
+            event.getPresentation()
+                    .setEnabledAndVisible(false);
         }
 
     }
@@ -44,49 +52,52 @@ public class ConvertAction extends AnAction {
         // get file type
         final String fileType = CommonUtils.getFileType(event, true);
         final PsiFile selectedFile = CommonUtils.getSelectedFile(event, true);
-        if (StringUtil.isEmpty(fileType) || null == selectedFile) {
+        if(StringUtil.isEmpty(fileType) || null == selectedFile) {
             return;
         }
         final VirtualFile file = selectedFile.getVirtualFile();
-        if (null == file) {
+        if(null == file) {
             Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.NO_FILE_SELECTED,
                     MsgConsts.SELECT_FILE_FIRST, NotificationType.ERROR));
             return;
         }
-        ApplicationManager.getApplication().runWriteAction(() -> {
-            try {
-                String content = new String(file.contentsToByteArray());
-                if (StringUtil.isEmpty(content)) {
-                    Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.FILE_NOT_EMPTY,
-                            MsgConsts.SELECT_FILE_FIRST, NotificationType.ERROR));
-                    return;
-                }
-                //YAML文件处理
-                if (Constant.YAML.equals(fileType)) {
-                    String yamlContent = Yaml2Props.fromContent(content).convert();
-                    file.setCharset(file.getCharset());
-                    file.rename(this, file.getNameWithoutExtension() + Constant.PROPERTIES_SUFFIX);
-                    file.setBinaryContent(yamlContent.getBytes());
+        ApplicationManager.getApplication()
+                .runWriteAction(() -> {
+                    try {
+                        String content = new String(file.contentsToByteArray());
+                        if(StringUtil.isEmpty(content)) {
+                            Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.FILE_NOT_EMPTY,
+                                    MsgConsts.SELECT_FILE_FIRST, NotificationType.ERROR));
+                            return;
+                        }
+                        // YAML文件处理
+                        if(Constant.YAML.equals(fileType)) {
+                            String yamlContent = Yaml2Props.fromContent(content)
+                                    .convert();
+                            file.setCharset(file.getCharset());
+                            file.rename(this, file.getNameWithoutExtension() + Constant.PROPERTIES_SUFFIX);
+                            file.setBinaryContent(yamlContent.getBytes());
 
-                    Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.SUCCESS,
-                            MsgConsts.YAML2PROPS, NotificationType.INFORMATION));
-                }//PROPERTIES文件处理
-                else if (Constant.PROPERTIES.equals(fileType)) {
-                    String propsContent = Props2Yaml.fromContent(content).convert();
-                    file.rename(this, file.getNameWithoutExtension() + Constant.YAML_SUFFIX);
-                    file.setBinaryContent(propsContent.getBytes());
-                    Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.SUCCESS,
-                            MsgConsts.PROPS2YAML, NotificationType.INFORMATION));
-                } else {
-                    Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.INCORRECT_FILE_SELECTED,
-                            MsgConsts.SELECT_PROPS_OR_YAML_FIRST, NotificationType.ERROR));
-                }
+                            Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.SUCCESS,
+                                    MsgConsts.YAML2PROPS, NotificationType.INFORMATION));
+                        }// PROPERTIES文件处理
+                        else if(Constant.PROPERTIES.equals(fileType)) {
+                            String propsContent = Props2Yaml.fromContent(content)
+                                    .convert();
+                            file.rename(this, file.getNameWithoutExtension() + Constant.YAML_SUFFIX);
+                            file.setBinaryContent(propsContent.getBytes());
+                            Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.SUCCESS,
+                                    MsgConsts.PROPS2YAML, NotificationType.INFORMATION));
+                        } else {
+                            Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID, MsgConsts.INCORRECT_FILE_SELECTED,
+                                    MsgConsts.SELECT_PROPS_OR_YAML_FIRST, NotificationType.ERROR));
+                        }
 
-            } catch (IOException e) {
-                Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID,
-                        MsgConsts.CANNOT_RENAME_FILE, e.getMessage(), NotificationType.ERROR));
-            }
-        });
+                    } catch(IOException e) {
+                        Notifications.Bus.notify(new Notification(Constant.GROUP_DISPLAY_ID,
+                                MsgConsts.CANNOT_RENAME_FILE, e.getMessage(), NotificationType.ERROR));
+                    }
+                });
     }
 
 }
